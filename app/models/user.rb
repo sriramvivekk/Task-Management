7cd159da40d122aa7805716726a40c028c2d2dd7
sriramvivekk <sriramvivekk@users.noreply.github.com>
2022-03-07 05:14:04 +0530

    Add validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :projects, dependent: :destroy
+
+  validates :email, format: { with: /[a-zA-Z0-9.-]+@[a-zA-Z-]+\.\w+/, message: "not Valid format, ex: 'test@gmail.com'" }
 end
